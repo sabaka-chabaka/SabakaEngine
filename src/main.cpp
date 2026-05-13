e73a91b557ab3a77157ec121d62926dcd83cb534
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-13 16:40:55 +0400

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
-int main() {
-    return 0;
+#include "core/Application.h"
+#include <Windows.h>
+
+int WINAPI WinMain(
+    _In_     HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_     LPSTR     lpCmdLine,
+    _In_     int       nShowCmd) {
+    try {
+        engine::core::Application app;
+        return app.run();
+    }
+    catch (const std::exception& e) {
+        MessageBoxA(nullptr, e.what(), "Error", MB_OK | MB_ICONERROR);
+        return -1;
+    }
 }
