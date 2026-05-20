5f4239066e5305fbae31dd337812678178db0209
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-20 22:47:10 +0400

    Component.h

---
+++ src/core/Component.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace engine::core {
+    class Component {
+    public:
+        virtual ~Component() = default;
+
+        virtual void onUpdate(float deltaTime);
+        virtual void onRender();
+
+        class Entity* owner = nullptr;
+    };
+}
