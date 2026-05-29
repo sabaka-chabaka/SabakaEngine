15ebeda39623d0c726a0e911553cc00335c64427
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-29 22:43:19 +0400

    handle

---
+++ src/assets/AssetHandle.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "assets/AssetId.h"
+#include <memory>
+
+namespace engine::assets {
+
+    template<typename T>
+    class AssetHandle {
+    public:
+        AssetHandle() = default;
+
+        AssetHandle(AssetId id, std::weak_ptr<T> ptr)
+            : m_id(id), m_ptr(std::move(ptr)) {}
+
+        AssetId getId() const {
+            return m_id;
+        }
+
+        bool isValid() const {
+            return engine::asset::isValid(m_id) && !m_ptr.expired();
+        }
+
+        std::shared_ptr<T> lock() const {
+            return m_ptr.lock();
+        }
+
+        void reset() {
+            m_id = INVALID_ASSET_ID;
+            m_ptr.reset();
+        }
+
+        bool operator==(const AssetHandle<T>& other) const {
+            return m_id == other.m_id;
+        }
+
+        bool operator!=(const AssetHandle<T>& other) const {
+            return m_id != other.m_id;
+        }
+
+        explicit operator bool() const {
+            return isValid();
+        }
+
+    private:
+        AssetId            m_id  = INVALID_ASSET_ID;
+        std::weak_ptr<T>   m_ptr;
+    };
+}
